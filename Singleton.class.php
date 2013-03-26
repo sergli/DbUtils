f67c51da59ccfc170eeaae07e7dd2c3927829a17
Sergey Lisenkov <sergli@nigma.ru>
2013-03-26 22:35:26 +0400

    trait Singleton

---
+++ Singleton.class.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace db_utils;
+
+trait Singleton {
+
+	private static $_instances = [];
+
+	final public static function getInstance($tag = 0) {
+		$tag = abs($tag);
+		if (!isset(static::$_instances[$tag])) {
+			static::$_instances[$tag] = new static;
+		}
+
+		return static::$_instances[$tag];
+	}
+
+	final private function __construct() {
+		static::_init();
+	}
+
+	abstract protected static function _init();
+
+
+	final private function __clone() {
+	}
+
+	final private function __sleep() {
+	}
+
+	final private function __wakeup() {
+	}
+}
