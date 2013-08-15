002c0b8c2ccd37d4ce3ed8ee4bec3e95e9d7cb08
Sergey Lisenkov <sergli@nigma.ru>
2013-08-15 20:21:38 +0400

    Добавил config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = [
+	'mysql'	=>	[
+		'host'		=>	'localhost',
+		'user'		=>	'root',
+		'password'	=>	'Chipikavoc5',
+		'dbname'	=>	'clicklog',
+	],
+	'postgres'	=>	[
+		'host'		=>	'127.0.0.1',
+		'user'		=>	'sergli',
+		'password'	=>	'Chipikavoc5',
+		'dbname'	=>	'sergli',
+	]
+];
+
+return $config;
+
