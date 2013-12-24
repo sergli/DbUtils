f7720fefa88b93a9d761c0be44f45a6392d52d09
Sergey Lisenkov <sergli@nigma.ru>
2013-12-24 18:32:25 +0400

    iSelect почему-то был классов. Исправил на интерфейс

--- select/iSelect.class.php
+++ select/iSelect.class.php
@@ -4,28 +4,25 @@ namespace db_utils\select;
 
 /**
  * Интерфейс запроса, возвращающего набор записей
- * 
- * @abstract
- * @author Sergey Lisenkov <sergli@nigma.ru> 
+ *
+ * @author Sergey Lisenkov <sergli@nigma.ru>
  */
-abstract class iSelect implements \IteratorAggregate, \Countable {
-	
+interface iSelect extends \IteratorAggregate, \Countable {
+
 	/**
 	 * Освободить ресурсы
-	 * 
-	 * @abstract
+	 *
 	 * @access public
 	 * @return void
 	 */
-	public abstract function free();
+	public function free();
 
 	/**
 	 * Вернуть внутренний экземпляр запроса
-	 * 
-	 * @abstract
+	 *
 	 * @access public
 	 * @return mixed mysqli_result, resource, etc
 	 */
-	public abstract function getResult();
+	public function getResult();
 
 }
