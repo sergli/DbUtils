29ea1431d0fd1ba5d503bd3360237e11a36636ff
Sergey Lisenkov <sergli@nigma.ru>
2014-02-12 21:17:26 +0400

    тесты для классов Select

---
+++ Tests/DbUtils/Adapter/Pdo/PdoPgsqlSelectTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DbUtils\Adapter\Pdo;
+
+class PdoMysqlSelectTest extends \PHPUnit_Framework_TestCase
+{
+	private $_select;
+
+	public function setUp()
+	{
+		$db = (new \DbUtils\DiContainer)['db.pdo_pgsql'];
+		$sql = "select * from information_schema.tables
+		where table_schema='information_schema' limit 20";
+		$this->_select = $db->query($sql);
+	}
+
+	public function testGetResource()
+	{
+		$this->assertInstanceOf(
+			'\DbUtils\Adapter\SelectInterface', $this->_select);
+		$this->assertInstanceOf('\PDOStatement',
+			$this->_select->getResource());
+	}
+
+	public function testTraverse()
+	{
+		$this->assertContainsOnly('array', $this->_select);
+	}
+
+	public function testCount()
+	{
+		$this->assertCount(20,
+			iterator_to_array($this->_select));
+		$this->assertCount(20, $this->_select);
+	}
+}
