b2879d53430111f7b07e5b8b3604831cc9f0bf02
Sergey Lisenkov <sergli@nigma.ru>
2014-02-12 18:27:35 +0400

    Переобозначил ключи в DiContainer

--- src/DbUtils/DiContainer.php
+++ src/DbUtils/DiContainer.php
@@ -16,60 +16,61 @@ use Monolog\Processor\MemoryPeakUsageProcessor;
 use Monolog\Processor\MemoryUsageProcessor;
 
 
-class DiContainer extends Pimple {
-
-	public function __construct() {
-
+class DiContainer extends Pimple
+{
+	public function __construct()
+	{
 		parent::__construct();
 
-		$this['config'] = function($ci) {
+		//	весь массив конфига
+		$this['config'] = function($ci)
+		{
 			return require __DIR__ . '/config.php';
 		};
 
 		//	новое соединение с mysql по mysqli (force-new)
-		$this['mysql-new'] = $this->factory(function($ci) {
+		$this['db.mysql.force'] = $this->factory(function($ci)
+		{
 			return new Mysqli($ci['config']['mysql']);
 		});
 
 		//	синглтон mysqli
-		$this['mysql'] = function($ci) {
-			return $ci['mysql-new'];
+		$this['db.mysql'] = function($ci)
+		{
+			return $ci['db.mysql.force'];
 		};
 
 		//	новое соединение с postgres по php_pgsql
-		$this['postgres-new']= $this->factory(function($ci) {
+		$this['db.pgsql.force']= $this->factory(function($ci)
+		{
 			return new Pgsql($ci['config']['postgres']);
 		});
 
 		//	синглтон php_pgsql
-		$this['postgres'] = function($ci) {
-			return $ci['postgres-new'];
+		$this['db.pgsql'] = function($ci)
+		{
+			return $ci['db.pgsql'];
 		};
 
-		$this['postgres-wiki'] = function($ci) {
-			$config = $ci['config']['postgres'];
-			$config['dbname'] = 'wiki';
-			return new Pgsql($config);
-		};
-
-		$this['monolog'] = function($ci) {
+		//	логгер по умолчанию, INFO-only
+		$this['monolog'] = function($ci)
+		{
 			$logger = new Logger('DbUtils');
-			$logger->pushHandler(
-				new StreamHandler('php://stderr',
-				\Monolog\Logger::INFO));
 			$logger->pushProcessor(new MemoryPeakUsageProcessor);
 
 			return $logger;
 		};
 
-		$this['pdo-mysql'] = function($ci) {
+		//	синглтон pdo_mysql
+		$this['db.pdo_mysql'] = function($ci)
+		{
 			return new Pdo\Mysql($ci['config']['mysql']);
 		};
 
-		$this['pdo-postgres'] = function($ci) {
+		//	синглтон pdo_pgsql
+		$this['db.pdo_pgsql'] = function($ci)
+		{
 			return new Pdo\Pgsql($ci['config']['postgres']);
 		};
-
-		$this['pdo-pgsql'] = $this->raw('pdo-postgres');
 	}
 }
