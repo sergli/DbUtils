58f5145ecaf6df078f11612e644857816955f0ce
Sergey Lisenkov <sergli@nigma.ru>
2014-02-24 19:17:43 +0400

    тесты pg_copy_from приведены в соотв-ий шаблону вид

---
+++ tests/DbUtils/Tests/Saver/Postgres/PgCopyFrom/PgsqlRealtimeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DbUtils\Tests\Saver\Postgres\PgCopyFrom;
+
+use \DbUtils\Adapter\Pgsql\Pgsql as Adapter;
+use \DbUtils\Saver\Postgres\PgCopyFromSaver as Saver;
+
+class PgsqlRealtimeTest extends
+	\PHPUnit_Extensions_Database_TestCase
+{
+	use \DbUtils\Tests\Saver\RealtimeTestsTrait;
+
+	protected function _newPdo(array $config)
+	{
+		$config = $config['postgres'];
+		$dsn = sprintf('pgsql:host=%s;dbname=%s',
+			$config['host'], $config['dbname']);
+		$pdo = new \PDO($dsn,
+			$config['user'],
+			$config['password']);
+		$pdo->query('SET client_encoding TO UTF8');
+
+		return $pdo;
+	}
+
+	protected function _newAdapter(array $config)
+	{
+		return new Adapter($config['postgres']);
+	}
+
+	protected function _newSaver($db, $tableName)
+	{
+		return new Saver($db, $tableName);
+	}
+}
